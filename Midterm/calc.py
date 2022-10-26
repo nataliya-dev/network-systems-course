44fb470fb8af114a0ae8cca3c649f0f06a2a6891
Nataliya <23035717+nataliya-dev@users.noreply.github.com>
2022-10-26 16:15:52 -0600

    rate based on pixels

--- Midterm/calc.py
+++ Midterm/calc.py
@@ -6,12 +6,29 @@ population = 32.0*m.pow(10.0, 6)
 percent_users = 0.25
 total_clients = population*percent_users
 print("total_clients: ", total_clients)
-file_size = 287.6*8*m.pow(10, 6)  # bits
-print("file_size: ", file_size)
-file_len = 60.0  # seconds
 
-req_band = (file_size)/(file_len)*total_clients
-print("req_band: ", req_band)
+img = 1080*1920*24*0.1
+print("img: ", img)
+
+audio = 1.0/60.0*(m.pow(10.0, 6))*0.1
+print("audio: ", audio)
+
+
+frame_size = img+audio
+frame_size += frame_size*0.027
+print("frame_size bits: ", frame_size)
+print("frame_size MB: ", frame_size/8.0/(m.pow(10.0, 6)))
+
+fps = 30.0
+pp_band = fps*frame_size
+print("pp_band bits: ", pp_band/8.0/(m.pow(10.0, 6)))
+
+req_band = pp_band*total_clients
+print("frame_size bits: ", frame_size)
+
+
+print("req_band: bps", req_band)
+print("req_band: GBps", req_band/8.0/(m.pow(10.0, 6)))
 
 R = 10.0*m.pow(10, 9)  # bits/sec
 print("R: ", R)
@@ -21,15 +38,11 @@ print("req_servers: ", req_servers)
 servers_per_datacenter = 10000.0
 num_data_centers = req_servers/servers_per_datacenter
 print("num_data_centers: ", num_data_centers)
+print("num cities: ", num_data_centers/3)
 
-RTT = 2.0/1000  # s
-S = 2.3*m.pow(10, 8)  # m/s
+
+RTT = 2.0/1000.0  # s
+S = 3.0*m.pow(10, 8)  # m/s
 M = RTT*S  # meters
 M = M/1000.0  # km
 print("M: ", M)
-
-# could probably have smaller cities
-# Toluca
-# Puebla
-# Cuernavaca
-# Tezontepec
